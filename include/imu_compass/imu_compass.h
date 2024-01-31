bfc0e9b1bf12b74bd5315ec1d2e595d112e969ed
Restyled.io <commits@restyled.io>
2024-01-31 12:05:14 +0000

    Restyled by astyle

--- include/imu_compass/imu_compass.h
+++ include/imu_compass/imu_compass.h
@@ -34,57 +34,57 @@ Header file for IMU Compass Class that combines gyroscope and magnetometer data 
 class IMUCompass {
 
 private:
-  ros::NodeHandle node_;
-  ros::NodeHandle private_node_;
-  ros::Subscriber imu_sub_;
-  ros::Subscriber mag_sub_;
-  ros::Subscriber decl_sub_;
-  ros::Publisher imu_pub_;
-  ros::Publisher mag_pub_;
-  ros::Publisher compass_pub_;
-  ros::Publisher raw_compass_pub_;
+    ros::NodeHandle node_;
+    ros::NodeHandle private_node_;
+    ros::Subscriber imu_sub_;
+    ros::Subscriber mag_sub_;
+    ros::Subscriber decl_sub_;
+    ros::Publisher imu_pub_;
+    ros::Publisher mag_pub_;
+    ros::Publisher compass_pub_;
+    ros::Publisher raw_compass_pub_;
 
-  tf::TransformListener listener_;
-  ros::Timer debug_timer_;
+    tf::TransformListener listener_;
+    ros::Timer debug_timer_;
 
-  void imuCallback(sensor_msgs::ImuPtr data);
-  void declCallback(const std_msgs::Float32& data);
-  void magCallback(const geometry_msgs::Vector3StampedConstPtr& data);
-  void debugCallback(const ros::TimerEvent&);
-  void repackageImuPublish(tf::StampedTransform);
+    void imuCallback(sensor_msgs::ImuPtr data);
+    void declCallback(const std_msgs::Float32& data);
+    void magCallback(const geometry_msgs::Vector3StampedConstPtr& data);
+    void debugCallback(const ros::TimerEvent&);
+    void repackageImuPublish(tf::StampedTransform);
 
-  //Heading Filter functions
-  void initFilter(double heading_meas); //initialize heading fiter
-  bool first_mag_reading_; //signifies receiving the first magnetometer message
-  bool first_gyro_reading_; //signifies receiving the first gyroscope message
-  bool filter_initialized_; //after receiving the first measurement, make sure the filter is initialized
-  bool gyro_update_complete_; //sigfnifies that a gyro update (motion model update) has gone through
+    //Heading Filter functions
+    void initFilter(double heading_meas); //initialize heading fiter
+    bool first_mag_reading_; //signifies receiving the first magnetometer message
+    bool first_gyro_reading_; //signifies receiving the first gyroscope message
+    bool filter_initialized_; //after receiving the first measurement, make sure the filter is initialized
+    bool gyro_update_complete_; //sigfnifies that a gyro update (motion model update) has gone through
 
-  std::string base_frame_;
+    std::string base_frame_;
 
-  double mag_zero_x_, mag_zero_y_, mag_zero_z_;
+    double mag_zero_x_, mag_zero_y_, mag_zero_z_;
 
-  sensor_msgs::ImuPtr curr_imu_reading_;
+    sensor_msgs::ImuPtr curr_imu_reading_;
 
-  //Heading Filter Variables
-  //State and Variance
-  double curr_heading_;
-  double curr_heading_variance_;
-  double sensor_timeout_;
+    //Heading Filter Variables
+    //State and Variance
+    double curr_heading_;
+    double curr_heading_variance_;
+    double sensor_timeout_;
 
-  //Motion Update Variables
-  double heading_prediction_;
-  double heading_variance_prediction_;
-  double heading_prediction_variance_;
-  double mag_declination_;
-  double last_motion_update_time_;
-  double last_measurement_update_time_;
+    //Motion Update Variables
+    double heading_prediction_;
+    double heading_variance_prediction_;
+    double heading_prediction_variance_;
+    double mag_declination_;
+    double last_motion_update_time_;
+    double last_measurement_update_time_;
 
-  //Measurement Update Variables
-  double yaw_meas_variance_;
+    //Measurement Update Variables
+    double yaw_meas_variance_;
 
 public:
-  IMUCompass(ros::NodeHandle &n, ros::NodeHandle &pn);
-  ~IMUCompass() {
-  }
+    IMUCompass(ros::NodeHandle &n, ros::NodeHandle &pn);
+    ~IMUCompass() {
+    }
 };
